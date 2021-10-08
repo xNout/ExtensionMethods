51011e243af8d853e103260459c5db55ee3f6ebb
Adrian Vera <adrian.vera@guru-soft.com>
2021-10-07 21:20:43 -0500

    Mail Service

---
+++ ExtensionMethods/MailService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+
+namespace ExtensionMethods
+{
+    public class MailService
+    {
+        public static bool IsValid(string emailaddress)
+        {
+            if (emailaddress == "")
+                emailaddress = "d";
+
+            try
+            {
+                MailAddress m = new MailAddress(emailaddress);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+
+        }
+    }
+}
--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -6,7 +6,13 @@ namespace ExtensionMethods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+            string Mail = "mimail@gmail.com";
+
+            bool esValido = MailService.IsValid(Mail);
+
+            Console.WriteLine("El mail es valido: " + (esValido ? "SI" : "NO"));
         }
     }
 }
